857ceb214ade748c7364d4c882adb8da1a969b40
Iqbal Hossan <engiqbal1999@gmail.com>
2021-05-11 10:39:46 +0600

    null vs undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,16 @@
+// let name;
+// console.log(name);
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// const result = add(13, 20);
+// console.log(result);
+
+let person = {
+    name: "Iqbal",
+    roll: "939510"
+}
+console.log(person.depertment);
